f49c594e95d189a31965ae22444f8cf41cba738e
felixmaechtle <f.maechtle@ingeniumdesign.de>
2019-04-15 17:10:08 +0200

    first commit

---
+++ src/main/java/felix/java2uml/App.java
@@ -0,0 +1,159 @@
+package felix.java2uml;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import net.sourceforge.plantuml.FileFormat;
+import net.sourceforge.plantuml.FileFormatOption;
+import net.sourceforge.plantuml.SourceStringReader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends Application
+{
+	static FileFinder f;
+	Label lbl = new Label("Drag a Folder an the Screen");
+	
+    public static void main( String[] args )
+    {
+    	
+    	Application.launch(args);
+    	// String svg = generate(args[0]);
+    	
+    	//System.out.println(svg);
+    }
+    
+    
+    public static String generate(String path) {
+    	
+    	f = new FileFinder(path);
+        f.generateUml();
+    	
+        String svg = "";
+    	
+    	// Write the first image to "os"
+    	try {
+    		SourceStringReader reader = new SourceStringReader(f.getUml());
+        	final ByteArrayOutputStream os = new ByteArrayOutputStream();
+			String desc = reader.generateImage(os, new FileFormatOption(FileFormat.SVG));
+			os.close();
+			// The XML is stored into svg
+	    	svg = new String(os.toByteArray(), Charset.forName("UTF-8"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "";
+		}
+    	
+    	return svg;
+    }
+
+
+	@Override
+	public void start(Stage stage) throws Exception {
+ 
+        // Create the GridPane
+        GridPane pane = new GridPane();
+        pane.setHgap(5);
+        pane.setVgap(20);
+     
+        // Create the VBox
+        VBox root = new VBox();
+ 
+        root.setOnDragOver(new EventHandler<DragEvent>() {
+
+            @Override
+            public void handle(DragEvent event) {
+                if (event.getGestureSource() != root
+                        && event.getDragboard().hasFiles()) {
+                    /* allow for both copying and moving, whatever user chooses */
+                    event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+                }
+                event.consume();
+            }
+        });
+
+        root.setOnDragDropped(new EventHandler<DragEvent>() {
+
+            @Override
+            public void handle(DragEvent event) {
+                Dragboard db = event.getDragboard();
+                boolean success = false;
+                if (db.hasFiles()) {
+                	
+                	lbl.setText("Where should the Diagram be saved?");
+                	FileChooser fileChooser = new FileChooser();
+                	 
+                    //Set extension filter for text files
+                    FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("SGV-Graphics files (*.svg)", "*.svg");
+                    fileChooser.getExtensionFilters().add(extFilter);
+         
+                    //Show save file dialog
+                    File file = fileChooser.showSaveDialog(stage);
+         
+                    if (file != null) {
+                    	lbl.setText("processing");
+                        saveTextToFile(generate(db.getFiles().get(0).toString()), file);
+                    }
+                    lbl.setText("done.");
+                }
+                /* let the source know whether the string was successfully 
+                 * transferred and used */
+                event.setDropCompleted(success);
+
+                event.consume();
+            }
+        });
+ 
+         // Set the Style of the VBox
+        root.setStyle("-fx-padding: 10;" +
+            "-fx-border-style: solid inside;" +
+            "-fx-border-width: 2;" +
+            "-fx-border-insets: 5;" +
+            "-fx-border-radius: 5;" +
+            "-fx-border-color: blue;");
+        
+        
+        lbl.setFont(Font.font("", FontWeight.NORMAL, 24));
+        root.getChildren().add(lbl);
+
+        // Create the Scene
+        Scene scene = new Scene(root,600,400);
+        
+        // Add the Scene to the Stage
+        stage.setScene(scene);
+        // Set the Title
+        stage.setTitle("Java to UML");
+        // Display the Stage
+        stage.show();
+	}
+	
+	private void saveTextToFile(String content, File file) {
+        try {
+            PrintWriter writer;
+            writer = new PrintWriter(file);
+            writer.println(content);
+            writer.close();
+        } catch (IOException ex) {
+            
+        }
+    }
+}
---
+++ src/main/java/felix/java2uml/FileFinder.java
@@ -0,0 +1,247 @@
+package felix.java2uml;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.EnumConstantDeclaration;
+import com.github.javaparser.ast.body.EnumDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+
+public class FileFinder {
+	
+	String path;
+	Stream<Path> files;
+	HashMap<String, String> packages;
+	String uml;
+	String currentWrapper = "";
+	String currentMethods = "";
+	String currentVars = "";
+	String currentEnumConstants = "";
+	
+	public FileFinder(String path) {
+		this.path = path;
+	}
+	
+	public void findFiles() {
+		try {
+			files = Files.walk(Paths.get(path))
+			.filter(Files::isRegularFile)
+			.filter( file -> file.toString().endsWith(".java") && !file.toString().endsWith("module-info.java"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void analyseFiles() {
+		if (files == null)
+			this.findFiles();
+		
+		packages = new HashMap<String, String>();
+		files.forEach(file -> analyseFile(file));
+	}
+	
+	public void generateUml() {
+		if (files == null)
+			analyseFiles();
+		
+		// Finde den Ordner, den alle noch innehaben
+		String basePath = "";
+		String onePath = "";
+		do {
+			boolean exitLoop = false;
+			
+			// Füge inkrementiv einen neuen Ordner hinzu
+			for(String path : packages.keySet()) {
+				if (!path.startsWith(basePath)) {
+					exitLoop = true;
+					basePath = basePath.substring(0, basePath.lastIndexOf("/"));
+				}
+				
+				onePath = path;
+			}
+			
+			if (exitLoop)
+				break;
+			
+			onePath = onePath.substring(basePath.length());
+			onePath = onePath.substring(0, onePath.indexOf("/") + 1); // TODO Windows
+			basePath += onePath;
+		} while(packages.keySet().size() != 0);
+		
+		basePath = basePath.substring(0, basePath.lastIndexOf("/")); // Der letzte Teil soll noch als Verbindung dran sein
+		
+
+		uml = "@startuml \n\n";
+		// Ordne alles in Packages an:
+		for(String path : packages.keySet()) {
+			
+			String packageName = path.substring(basePath.length() + 1).replace("/", ".");
+			packageName = packageName.substring(0, packageName.length() - 1);
+			
+			uml += "package " + packageName + " {\n"
+					+ packages.get(path) + "\n"
+					+ "}\n";
+		}
+		
+		uml += "@enduml";
+	}
+	
+	public void analyseFile(Path f) {
+		String file = "";
+		
+		// Read the file
+		try {
+			file = new String(Files.readAllBytes(f));
+		} catch (IOException e) {
+			System.out.println("Fehler beim Lesen von " + f.toString());
+			e.printStackTrace();
+			return;
+		}
+		
+		currentWrapper = "";
+		currentMethods = "";
+		currentVars = "";
+		currentEnumConstants = "";
+		
+		// Finde alle Elemente der Klasse
+		new VoidVisitorAdapter<Object>() {
+			
+			/**
+			 * Eine Klasse oder ein Interface
+			 */
+            @Override
+            public void visit(ClassOrInterfaceDeclaration n, Object arg) {
+                super.visit(n, arg);
+                
+                boolean isAbstract = n.isAbstract();
+                boolean isInterface = n.isInterface();
+                String name = n.getNameAsString();
+                String extendsString = n.getExtendedTypes().size() == 1 ? 
+                		" extends " + n.getExtendedTypes().get(0).getNameAsString() : "";
+                
+                String[] interfaces = new String[n.getImplementedTypes().size()];
+                for(int i = 0; i < n.getImplementedTypes().size() - 1; i++) {
+                	interfaces[i] = n.getImplementedTypes().get(i).asString();
+                }
+                
+                String implementsString = n.getImplementedTypes().size() > 0 ? 
+                		" implements " + String.join(", ", interfaces) : "";
+                		
+                currentWrapper =
+                		(isAbstract ? "abstract " : "") +
+                		(isInterface ? "interface " : "class ") +
+                		name +
+                		extendsString + 
+                		implementsString;
+            }
+            
+            
+            /**
+             * Ein Enum
+             */
+            @Override 
+            public void visit(EnumDeclaration n, Object arg) {
+                super.visit(n, arg);
+                
+                currentWrapper = 
+                		"enum " + n.getNameAsString();
+            }
+            
+            /**
+             * Eine enum-Konstante
+             */
+            @Override 
+            public void visit(EnumConstantDeclaration n, Object arg) {
+                super.visit(n, arg);
+                
+                currentEnumConstants += "    " + n.getNameAsString() + "\n"; 
+            }
+           
+            
+            /**
+             * Eine normale Funktion
+             */
+            @Override
+            public void visit(MethodDeclaration n, Object arg) {
+                super.visit(n, arg);
+                
+                String visibility = getAccesibility(n.getAccessSpecifier().toString());
+                String returnType = n.getTypeAsString();
+                String name = n.getNameAsString();
+                
+                String[] parameters = new String[n.getParameters().size()];
+                for(int i = 0; i < n.getParameters().size() - 1; i++) {
+                	parameters[i] = 
+                			n.getParameters().get(i).getTypeAsString() + " " + n.getParameters().get(i).getNameAsString();
+                }
+                String paramStr = Stream.of(parameters)
+                        .filter(s -> s != null && !s.isEmpty())
+                        .collect(Collectors.joining(", "));
+                
+                // Baue alles zusammen
+                currentMethods += "    " +
+                		visibility + 
+                		name + 
+                		"(" + paramStr + "):" + 
+                		returnType + "\n";
+            }
+           
+            /***
+             * Ein Atribut
+             */
+            @Override
+            public void visit(FieldDeclaration n, Object arg)
+            {   
+            	String visibility = getAccesibility(n.getAccessSpecifier().toString());
+            	String varName = n.getVariable(0).getNameAsString();
+            	String typus = n.getVariable(0).getTypeAsString();
+            	
+            	currentVars += "    " + visibility + varName + ": " + typus + "\n";
+            }
+        }.visit(JavaParser.parse(file), null);
+        
+        if (!currentWrapper.isEmpty()) {
+        	String key = new File(f.toString()).getAbsolutePath();
+        	key = key.substring(0, key.lastIndexOf("/") + 1);
+        	if (!packages.containsKey(key))
+        		packages.put(key, "");
+        	
+        	String packageUml = packages.get(key);
+        	packageUml += currentWrapper + " { \n" +
+	        		currentVars +
+	        		currentEnumConstants + 
+	        		currentMethods + 
+	        		"}\n\n";
+        	
+        	//System.out.println(key);
+        	packages.put(key, packageUml);
+        }
+	}
+
+	/**
+	 * @return the uml
+	 */
+	public String getUml() {
+		return uml;
+	}
+
+	private String getAccesibility(String accessibility) {
+		if (accessibility.equals("PRIVATE"))
+			return "-";
+		if (accessibility.equals("PUBLIC"))
+			return "+";
+		
+		return "FEHLER! : " + accessibility;
+	}
+}
